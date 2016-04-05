b3dbe40f5838dd7436b0c4a2cdd7e80269289984
2er0 <2er0@users.noreply.github.com>
2016-04-05 12:45:45 +0200

    fixed the tsc-transcode errors in angular 2 and a requier error

--- app/app.ts
+++ app/app.ts
@@ -1,3 +1,5 @@
+///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
+
 import {bootstrap} from 'angular2/platform/browser';
 import {Component/*, View*/, NgZone} from 'angular2/core';
 import {NgFor} from 'angular2/common';
