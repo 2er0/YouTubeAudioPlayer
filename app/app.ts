80baa4c20fc2198e52d313fafff3af632e82f024
2er0 <2er0@users.noreply.github.com>
2016-04-06 00:43:31 +0200

    updated html struct to tap design

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,10 @@
 ///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
 
 import {bootstrap} from 'angular2/platform/browser';
-import {Component/*, View*/, NgZone} from 'angular2/core';
+import {Component/*, View*/, NgZone, ViewEncapsulation} from 'angular2/core';
 import {NgFor} from 'angular2/common';
+import {MATERIAL_DIRECTIVES} from "ng2-material/all";
+
 import {Resultlist} from './components/resultlist/resultlist';
 import {Search} from './components/search/search';
 import {Player} from './components/player/player';
@@ -21,22 +23,39 @@ const ipc = electron.ipcRenderer;
 
 @Component({
   selector: 'app',
-  directives: [Search, Resultlist, Player, Sidebar],
-  template: `
-    <div id="SearchAndResult" flex="80">
-      <search></search>
-      <resultlist></resultlist> 
-    </div>
-    <sidebar id="SidebarMain" flex=20></sidebar>
+  directives: [Search, Resultlist, Player, Sidebar, MATERIAL_DIRECTIVES],
+  encapsulation: ViewEncapsulation.None,
+  template: ` 
+    <md-content id="tabing">
+      <md-tabs md-dynamic-height md-border-bottom>
+        <template md-tab label="YouTube Audio">
+          <md-content class="md-padding">
+            <search></search>
+            <resultlist></resultlist> 
+          </md-content>
+        </template>
+        <template md-tab label="Settings">
+          <md-content class="md-padding">
+            <sidebar id="SidebarMain"></sidebar>
+          </md-content>
+        </template>
+      </md-tabs>
+    </md-content>
     <player id="PlayerMain"></player>
   ` ,
   styles: [`
-    #SearchAndResult {
+    #tabing {
       height: calc(100% - 90px);
+      width: 100%;
+      overflow: hidden;
+    }
+    search {
+      width: 100%;
+      height: 76px;
     }
     #SidebarMain {
-      /*max-height: 270px;*/
-      height: calc(100% - 90px);
+      /*max-height: 270px;
+      height: calc(100% - 90px);*/
     }
     #PlayerMain {
       margin: 5px;
